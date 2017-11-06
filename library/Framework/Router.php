8add772de62b59b581dcf1fefb607b62c6218766
Nick Miller <nick@nmiller.info>
2017-11-05 21:57:09 -0500

    (#2) initial Router implementation

---
+++ library/Framework/Router.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Framework;
+
+use \Framework\Router\Route;
+use \Framework\Router\RouteMatch;
+
+final class Router extends Emitter
+{
+    const DEFAULT_PREFIX = "/";
+
+    private $prefix = self::DEFAULT_PREFIX;
+    private $routes = [];
+    private $started = false;
+
+    public function __construct($prefix = self::DEFAULT_PREFIX)
+    {
+        $this->prefix = (string)$prefix;
+    }
+
+    private function dispatch(string $fragment): void
+    {
+        $match = $this->matchFragment($fragment);
+        $route = $match->route;
+        $arguments = $match->arguments;
+
+        $route->invoke($arguments);
+    }
+
+    private function matchFragment(string $fragment): RouteMatch
+    {
+        foreach ($this->routes as $name => $route) {
+            $pattern = $route->getPattern();
+            $arguments = [];
+
+            if (preg_match($pattern, $fragment, $arguments)) {
+                return new RouteMatch($route, $arguments);
+            }
+        }
+
+        // TODO: sane default?
+        throw new Exception("Invalid route specified");
+    }
+
+    public function add(string $name, string $pattern, $handler): Router
+    {
+        [$class, $method] = $handler;
+        $this->routes[$name] = new Route($pattern, $class, $method);
+
+        return $this;
+    }
+
+    public function remove(string $name): Router
+    {
+        unset($this->routes[$name]);
+
+        return $this;
+    }
+
+    public function start(): Router
+    {
+        if (!$this->started) {
+            $this->dispatch($_SERVER["REQUEST_URI"]);
+            $this->started = true;
+        }
+
+        return $this;
+    }
+}
