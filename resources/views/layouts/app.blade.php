3bf331ad6b893ebc0ffa082dc26d24315892af64
Nick Miller <nick@nmiller.info>
2018-07-01 22:08:06 -0400

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>@yield('title') - nmiller.info</title>
     <link rel="stylesheet" href="/css/main.css">
+    <link rel="shortcut icon" href="/images/favicon.png">
 </head>
 <body>
     <header>
