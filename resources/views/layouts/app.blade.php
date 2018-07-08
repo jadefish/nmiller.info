30bb84c10aac72097cd02ae53427167bd71c4aa7
Nick Miller <nick@nmiller.info>
2018-07-08 19:56:41 -0400

    (#7) Add meta description

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Personal website of software engineer Nick Miller" />
     <title>@yield('title') - nmiller.info</title>
     <link rel="stylesheet" href="/css/main.css">
     <link rel="shortcut icon" href="/images/favicon.png">
